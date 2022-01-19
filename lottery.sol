89b7d4755af548b013b588e75b7b09ce4fe9999c
"chrop01" <"christoph.oprawill@outlook.com">
2022-01-19 20:17:52 +0100

    init commit

--- lottery.sol
+++ lottery.sol
@@ -2,4 +2,11 @@ pragma solidity >=0.5.0 <0.9.0;
 
 contract Lottery {
      
+     address public manager;
+     address payable[] public participiants;
+
+     constructor()
+     {
+         manager = msg.sender; //global
+     }
 }
